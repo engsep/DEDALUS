d43a981855b49cf770edd3dc2b7787d555452782
Smart Energy Projects <114106111+engsep@users.noreply.github.com>
2024-11-14 12:21:06 +0100

    Update test-write.sh

--- test-write.sh
+++ test-write.sh
@@ -2,7 +2,7 @@ curl -iX POST \
 'http://localhost:1026/ngsi-ld/v1/entities/' \
 -H 'X-Auth-token: 1234' \
 -H 'Content-Type: application/json' \
--H 'Link: <http://context:3000/data-models/ngsi-context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"' \
+-H 'Link: <http://context/data-models/ngsi-context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"' \
 --data-raw '{
       "id": "urn:ngsi-ld:TemperatureSensor:001",
       "type": "TemperatureSensor",
