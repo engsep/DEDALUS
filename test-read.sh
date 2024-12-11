5bc5f64c824f288ea5bf696f7dd64c180b025c8b
Piero Di Liberto <piero.diliberto@eng.it>
2024-12-11 17:05:44 +0100

    Remove unused files to clean up the project structure

--- test-read.sh
+++
@@ -1,5 +0,0 @@
-curl -G -iX GET \
-'http://localhost:1026/ngsi-ld/v1/entities/urn:ngsi-ld:TemperatureSensor:001' \
--H 'X-Auth-token: 1234' \
--H 'Link: <http://context/data-models/ngsi-context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"' \
--d 'options=sysAttrs'
