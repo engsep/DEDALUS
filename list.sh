89a8da76e32932d13f43942d3fd248f9c6aac063
engsep <smartenergyprojects.eng@gmail.com>
2024-12-20 12:41:39 +0000

    Healthchecks and container reference fixed

--- list.sh
+++ list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.ID}}" | sort
-docker ps -a --format "table {{.Networks}}\t{{.Names}}\t{{.RunningFor}}\t{{.Image}}\t{{.Command}}\t{{.Mounts}}"
-# docker ps -a --format "table {{.Names}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}"
+# docker ps -a --format "table {{.Networks}}\t{{.Names}}\t{{.RunningFor}}\t{{.Image}}\t{{.Command}}\t{{.Mounts}}"
+docker ps -a --format "table {{.Names}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}"
 
 # read enter
 # docker compose logs -n 10 -f
